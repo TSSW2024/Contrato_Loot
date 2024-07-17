460a3698565ef7a487300f71ee18ec7c8cfcd607
nicocapu <nicosep03mail.com>
2024-07-16 22:38:19 -0400

    modificacion de contrato loot

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"math"
-	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
-	"time"
+
+	"github.com/joho/godotenv"
 )
 
 type respuesta_binance struct {
@@ -25,35 +25,100 @@ type Monedas struct {
 	Probabilidad float64
 }
 
-type coingeckocoin struct {
-	Id     string `json:"id"`
-	Symbol string `json:"symbol"`
-	Name   string `json:"name"`
-	Image  string `json:"image"`
+type coinapicoin struct {
+	AssetID string `json:"asset_id"`
+	Name    string `json:"name"`
+	Url     string `json:"url"`
 }
 
-func obtener_datos_geckocoin() (map[string]coingeckocoin, error) {
-	url := "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
-	resp, err := http.Get(url)
+func obtener_datos_coinapi_icons(apiKey string) (map[string]string, error) {
+	url := "https://rest.coinapi.io/v1/assets/icons/55"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error al crear la solicitud HTTP: %v", err)
+	}
+	req.Header.Set("X-CoinAPI-Key", apiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("error al hacer la solicitud HTTP: %v", err)
 	}
 	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("error: respuesta HTTP %v", resp.StatusCode)
 	}
-	var coins []coingeckocoin
+
+	var coins []coinapicoin
 	if err := json.NewDecoder(resp.Body).Decode(&coins); err != nil {
-		return nil, fmt.Errorf("error al decodificar la respuesta JSON de CoinGecko: %v", err)
+		return nil, fmt.Errorf("error al decodificar la respuesta JSON de CoinAPI: %v", err)
 	}
-	coinMap := make(map[string]coingeckocoin)
+
+	iconMap := make(map[string]string)
 	for _, coin := range coins {
-		coinMap[strings.ToUpper(coin.Symbol)] = coin
+		iconMap[strings.ToUpper(coin.AssetID)] = coin.Url
+	}
+	return iconMap, nil
+}
+
+func obtener_datos_coinapi_assets(apiKey string) (map[string]string, error) {
+	url := "https://rest.coinapi.io/v1/assets"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error al crear la solicitud HTTP: %v", err)
+	}
+	req.Header.Set("X-CoinAPI-Key", apiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error al hacer la solicitud HTTP: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: respuesta HTTP %v", resp.StatusCode)
+	}
+
+	var coins []coinapicoin
+	if err := json.NewDecoder(resp.Body).Decode(&coins); err != nil {
+		return nil, fmt.Errorf("error al decodificar la respuesta JSON de CoinAPI: %v", err)
+	}
+
+	nameMap := make(map[string]string)
+	for _, coin := range coins {
+		nameMap[strings.ToUpper(coin.AssetID)] = coin.Name
+	}
+	return nameMap, nil
+}
+
+func obtener_datos_coinapi(apiKey string) (map[string]coinapicoin, error) {
+	icons, err := obtener_datos_coinapi_icons(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	names, err := obtener_datos_coinapi_assets(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	coinMap := make(map[string]coinapicoin)
+	for assetID, icon := range icons {
+		name, exists := names[assetID]
+		if exists {
+			coinMap[assetID] = coinapicoin{
+				AssetID: assetID,
+				Name:    name,
+				Url:     icon,
+			}
+		}
 	}
 	return coinMap, nil
 }
 
-func crear_monedas() ([]Monedas, error) {
+func crear_monedas(apiKey string) ([]Monedas, error) {
 	url := "https://api.binance.com/api/v3/ticker/price"
 	resp, err := http.Get(url)
 	if err != nil {
@@ -70,7 +135,7 @@ func crear_monedas() ([]Monedas, error) {
 		return nil, fmt.Errorf("error al decodificar la respuesta JSON: %v", err)
 	}
 
-	coinGeckoMap, err := obtener_datos_geckocoin()
+	coinAPIMap, err := obtener_datos_coinapi(apiKey)
 	if err != nil {
 		return nil, err
 	}
@@ -83,77 +148,94 @@ func crear_monedas() ([]Monedas, error) {
 				return nil, fmt.Errorf("error al convertir el precio a float: %v", err)
 			}
 			siglas := strings.TrimSuffix(datos.Symbol, "USDT")
-			coinData, exists := coinGeckoMap[siglas]
+			coinData, exists := coinAPIMap[siglas]
 			if !exists {
 				continue
 			}
 
-			ganancia := 1 / precio // Calcular ganancia como inversa del precio
+			ganancia := 1 / precio
 			moneda := Monedas{
-				Icono:        coinData.Image,
+				Icono:        coinData.Url,
 				Nombre:       coinData.Name,
 				Siglas:       datos.Symbol,
-				Ratio:        1 / float64(len(datos_binance)), // Ratio fijo, puedes ajustarlo según tu necesidad
+				Ratio:        1 / float64(len(datos_binance)),
 				Ganancia:     ganancia,
-				Probabilidad: ganancia, // Probabilidad proporcional a la ganancia
+				Probabilidad: ganancia,
 			}
 			monedas = append(monedas, moneda)
 		}
 	}
 
-	// Normalizar las probabilidades proporcionalmente a la ganancia
+	// Normalizar las probabilidades
 	sumaGanancias := 0.0
 	for _, moneda := range monedas {
 		sumaGanancias += moneda.Ganancia
 	}
 	for i := range monedas {
-		monedas[i].Probabilidad /= sumaGanancias
+		monedas[i].Probabilidad = monedas[i].Ganancia / sumaGanancias
 	}
 
-	// Seleccionar 10 monedas aleatorias sin repetición
-	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-	monedasAleatorias := make([]Monedas, 0, 10)
-	monedasSeleccionadas := make(map[int]bool)
-
-	for len(monedasAleatorias) < 10 {
-		indice := rng.Intn(len(monedas))
-		if !monedasSeleccionadas[indice] {
-			monedasAleatorias = append(monedasAleatorias, monedas[indice])
-			monedasSeleccionadas[indice] = true
+	return monedas, nil
+}
+func crear_caja_1(monedas []Monedas) []Monedas {
+	var caja []Monedas
+	for _, moneda := range monedas {
+		if moneda.Ganancia*100 < 1.1 {
+			caja = append(caja, moneda)
 		}
 	}
-
-	// Ajustar probabilidades para las 10 monedas seleccionadas
-	sumaProbabilidades := 0.0
-	for _, moneda := range monedasAleatorias {
-		sumaProbabilidades += moneda.Probabilidad
+	if len(caja) > 10 {
+		caja = caja[:10]
 	}
-	for i := range monedasAleatorias {
-		monedasAleatorias[i].Probabilidad /= sumaProbabilidades
-	}
-
-	// Retornar las monedas y ningún error
-	return monedasAleatorias, nil
+	return caja
 }
-
-func sumaProbabilidadesAproximada(monedas []Monedas) bool {
-	sumaProbabilidad := 0.0
+func crear_caja_2(monedas []Monedas) []Monedas {
+	var caja []Monedas
+	var valorAlto Monedas
 	for _, moneda := range monedas {
-		sumaProbabilidad += moneda.Probabilidad
+		if moneda.Ganancia*100 >= 80 {
+			caja = append(caja, moneda)
+		} else if moneda.Ganancia*100 < 0.5 {
+			valorAlto = moneda
+		}
 	}
-
-	return math.Abs(sumaProbabilidad-1.0) < 0.0001
+	if len(caja) > 9 {
+		caja = caja[:9]
+	}
+	if valorAlto != (Monedas{}) {
+		caja = append(caja, valorAlto)
+	}
+	return caja
 }
-
+func ajustar_probabilidades_caja(caja []Monedas) {
+	sumaGanancias := 0.0
+	for _, moneda := range caja {
+		sumaGanancias += moneda.Ganancia
+	}
+	for i := range caja {
+		caja[i].Probabilidad = caja[i].Ganancia / sumaGanancias
+	}
+}
 func handler(w http.ResponseWriter, r *http.Request) {
-	monedas, err := crear_monedas()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error al obtener las monedas de Binance: %v", err), http.StatusInternalServerError)
+	apiKey := os.Getenv("COINAPI_KEY")
+	if apiKey == "" {
+		http.Error(w, "Error: API key de CoinAPI no configurada", http.StatusInternalServerError)
 		return
 	}
 
-	for _, moneda := range monedas {
-		fmt.Fprintf(w, "Nombre: %s\n", moneda.Nombre)
+	monedas, err := crear_monedas(apiKey)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error al obtener las monedas: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	caja1 := crear_caja_1(monedas)
+	caja2 := crear_caja_2(monedas)
+	ajustar_probabilidades_caja(caja1)
+	ajustar_probabilidades_caja(caja2)
+
+	for _, moneda := range caja1 {
+		fmt.Fprintf(w, "Caja 1 - Nombre: %s\n", moneda.Nombre)
 		fmt.Fprintf(w, "Siglas: %s\n", moneda.Siglas)
 		fmt.Fprintf(w, "Icono: %s\n", moneda.Icono)
 		fmt.Fprintf(w, "Ganancia: %.8f%%\n", moneda.Ganancia*100)
@@ -162,14 +244,25 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "---")
 	}
 
-	if sumaProbabilidadesAproximada(monedas) {
-		fmt.Fprintln(w, "La suma de las probabilidades es aproximadamente igual a 1.")
-	} else {
-		fmt.Fprintln(w, "Error: La suma de las probabilidades no es aproximadamente igual a 1.")
+	for _, moneda := range caja2 {
+		fmt.Fprintf(w, "Caja 2 - Nombre: %s\n", moneda.Nombre)
+		fmt.Fprintf(w, "Siglas: %s\n", moneda.Siglas)
+		fmt.Fprintf(w, "Icono: %s\n", moneda.Icono)
+		fmt.Fprintf(w, "Ganancia: %.8f%%\n", moneda.Ganancia*100)
+		fmt.Fprintf(w, "Probabilidad: %.8f%%\n", moneda.Probabilidad*100)
+		fmt.Fprintf(w, "Ratio: %.8f\n", moneda.Ratio)
+		fmt.Fprintln(w, "---")
 	}
 }
 
 func main() {
+	// Cargar las variables de entorno desde el archivo .env
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error al cargar el archivo .env: %v\n", err)
+		return
+	}
+
 	http.HandleFunc("/", handler)
 	port := "8082"
 	fmt.Printf("Servidor escuchando en el puerto %s\n", port)
